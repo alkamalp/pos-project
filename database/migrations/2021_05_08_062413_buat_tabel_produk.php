c448c7fe705edccd7a122c35812a9f7c18caf1f6
alkamalp <62053797+alkamalp@users.noreply.github.com>
2021-05-09 02:36:41 +0700

    initial commit

---
+++ database/migrations/2021_05_08_062413_buat_tabel_produk.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuatTabelProduk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id_produk');
+            $table->string('nama_produk');
+            $table->integer('harga_jual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
